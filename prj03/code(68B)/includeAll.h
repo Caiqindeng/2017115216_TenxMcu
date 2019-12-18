17cd1357f5be8ace95bca5de50b169d626fec124
2017115216_TenxMcu <1730571674@qq.com>
2019-12-18 17:37:47 +0800

    全部头文件

--- prj03/code(68B)/includeAll.h
+++ prj03/code(68B)/includeAll.h
@@ -1,10 +1,12 @@
 #include <INTRINS.H>
-#include <REGtenxTM52F5268B.h>
+#include <REGtenxTM52F5278B.h>
 
 #include "typeAlias.h"
-#include "buzz.h"
+
 #include "display.h"
 #include "init.h"
 #include "isr.h"
 #include "keys.h"
 #include "main.h"
+#include "buzz.h"
+#include "temperature.h"
