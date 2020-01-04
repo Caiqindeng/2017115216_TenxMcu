331159318478fee9ed7597f48577d77985f1d3a1
2017115216_TenxMcu <1730571674@qq.com>
2020-01-04 22:31:36 +0800

    配置按键

--- prj03/code(68B)/keys.c
+++ prj03/code(68B)/keys.c
@@ -1,22 +1,103 @@
 #define __keys_c
+
 #include "includeAll.h"
+
 //=============================================================================
+
 void GetKeys() {
+
+	int delay = 0;
+
   static uint8_t tempKeyValue = D_keyNull;
 
-  //按键口 P10 设为上拉输入
-  P1MODL = 0xa8;
-  _nop_();
-  _nop_();
-  _nop_();
-  _nop_();
-  _nop_();
-  if (P_key1 == 0) {
+
+
+  if (P_key1 == 0||P_key2 == 0||P_key3 == 0||P_key4 == 0) {
+
     tempKeyValue = D_keyValue1;
+
+    
+
   } else {
+
     keyValue = tempKeyValue;
+
     tempKeyValue = D_keyNull;
+
   }
-  //按键口P10，恢复为输出 
-  P1MODL = 0xaa;
+
+	
+
+	if(P_key4 == 0){
+
+	key4 = 0; 
+
+	//led12 = 1;
+
+	}
+
+	//
+
+	
+
+}
+
+
+
+void GetKey1(){
+
+	//P_key1 euqal 0
+
+	if(P_key1 == 0){
+
+	key3 = 0; 
+
+	//led12 = 1;
+
+	}
+
+	
+
+}
+
+
+
+void GetKey2(){
+
+	if(P_key2 == 0&& key_flag == 1){
+
+		if((i+10)<100){	i=i+1;}
+
+		else{i=99;}
+
+		key_flag = 0;
+
+		led12 = 1;
+
+	}
+
+	//
+
+}
+
+
+
+
+
+void GetKey3(){
+
+	if(P_key3 == 0 && key_flag == 1){
+
+		if((i-10)>0){ i = i - 1;}
+
+		else{ i = 0; }
+
+		key_flag = 0;
+
+		led12 = 1;
+
+	}
+
+	//led12 = 1;
+
 }
--- prj03/code(68B)/keys.h
+++ prj03/code(68B)/keys.h
@@ -12,12 +12,22 @@
 // Globle Var -----------------------------------------
 #ifdef __keys_c
 uint8_t keyValue;
+bit key_flag;
 #else
 extern uint8_t keyValue;
+extern bit key_flag;
 #endif
 
 // Action Macro: exp: #define F_getData() ------
 
 // Function ------------------------------------
 void GetKeys();
+
+void GetKeys();
+
+void GetKey2();
+
+void GetKey3();
+
+void GetKey1();
 #endif
